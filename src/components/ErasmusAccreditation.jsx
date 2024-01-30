f1d229b5e40b4afaea0991a35e637ba5ac87e42b
Josip S <josip.skrlecvk@gmail.com>
2024-01-30 21:20:00 +0100

    OK

--- src/components/ErasmusAccreditation.jsx
+++ src/components/ErasmusAccreditation.jsx
@@ -1,68 +1,119 @@
-import React from "react";
+import React, { useState } from "react";
 import YearButton from "./YearButton";
 
 const ErasmusAccreditation = () => {
+  const [password, setPassword] = useState("");
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const checkPassword = () => {
+    const correctPassword = "mojlidac"; // Zamijenite sa stvarnom lozinkom
+    if (password === correctPassword) {
+      setIsAuthenticated(true);
+    } else {
+      alert("Pogrešna lozinka!");
+    }
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      checkPassword();
+    }
+  };
+
   return (
     <div className="container text-center">
       <h1>Erasmus Akreditacija - OŠ Vladimira Nazora, Vinkovci</h1>
       <p className="lead">2023. - 2027.</p>
-      {/* Ovdje koristite direktnu referencu na sliku iz public direktorija */}
       <img
         src={`${process.env.PUBLIC_URL}/Erasmus LOGO ŠKOLE.png`}
         alt="Erasmus Akreditacija Logo"
         style={{ maxWidth: "100%", height: "auto" }}
       />
-      <div className="d-flex flex-column">
-        <YearButton year="2023/2024" path="/2023-2024" />
-        <YearButton year="2024/2025" path="/2024-2025" />
-        <YearButton year="2025/2026" path="/2025-2026" />
-        <YearButton year="2026/2027" path="/2026-2027" />
+      {!isAuthenticated && (
+        <div>
+          <input
+            type="password"
+            value={password}
+            onChange={handlePasswordChange}
+            onKeyPress={handleKeyPress}
+            autoFocus
+            className="form-control form-control-lg mb-4"
+            placeholder=""
+            style={{ width: 'auto', maxWidth: '100%', display: 'inline-block' }}
+            />
+          <br />
+          <button
+            onClick={checkPassword}
+            className="btn btn-primary btn-lg rounded-pill"
+          >
+            Unesi lozinku
+          </button>
+        </div>
+      )}
 
-        <h2 className="font-weight-bold border-bottom mt-5">
-          Vanjske poveznice:
-        </h2>
-        <h3 className="mt-5">
-          <a href="http://ampeu.hr/" target="_blank" rel="noopener noreferrer">
-            AMPEU stranica
-          </a>
-        </h3>
-        <h3>
-          <a
-            href="https://webgate.ec.europa.eu/beneficiary-module/project/#/project-list"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Beneficiary Module
-          </a>
-        </h3>
-        <h3>
-          <a
-            href="http://os-vnazora-vk.skole.hr/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Web stranica škole
-          </a>
-        </h3>
-        <h3>
-          <a
-            href="https://www.facebook.com/Ekids2020/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Facebook stranica škole
-          </a>
-        </h3>
-        <h3>
-          <a
-            href="http://projekt.hr/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Web stranica projekta
-          </a>
-        </h3>
-      </div>
+      {isAuthenticated && (
+        <div>
+          <div className="d-flex flex-column">
+            <YearButton year="2023/2024" path="/2023-2024" />
+            <YearButton year="2024/2025" path="/2024-2025" />
+            <YearButton year="2025/2026" path="/2025-2026" />
+            <YearButton year="2026/2027" path="/2026-2027" />
+          </div>
+
+          <h2 className="font-weight-bold border-bottom mt-5">
+            Vanjske poveznice:
+          </h2>
+          <div className="mt-5">
+            <a
+              href="http://ampeu.hr/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              AMPEU stranica
+            </a>
+          </div>
+          <div>
+            <a
+              href="https://webgate.ec.europa.eu/beneficiary-module/project/#/project-list"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Beneficiary Module
+            </a>
+          </div>
+          <div>
+            <a
+              href="http://os-vnazora-vk.skole.hr/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Web stranica škole
+            </a>
+          </div>
+          <div>
+            <a
+              href="https://www.facebook.com/Ekids2020/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Facebook stranica škole
+            </a>
+          </div>
+          <div>
+            <a
+              href="http://projekt.hr/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Web stranica projekta
+            </a>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
--- src/components/Year2023To2024.jsx
+++ src/components/Year2023To2024.jsx
@@ -76,45 +76,87 @@ const Year2023To2024 = () => {
           <tr>
             <th scope="row">Mobilnosti nastavnika</th>
             <td>
-              <button className="btn btn-primary" style={{ marginRight: "20px" }} onClick={handleTenerifeClick}>Tenerife</button>
-              <button className="btn btn-primary" style={{ marginRight: "20px" }} onClick={handleBudimpeštaClick}>Budimpešta</button>
-              <button className="btn btn-primary" onClick={handleRimClick}>Rim</button>
+              <button
+                className="btn btn-primary"
+                style={{ marginRight: "20px" }}
+                onClick={handleTenerifeClick}
+              >
+                Tenerife
+              </button>
+              <button
+                className="btn btn-primary"
+                style={{ marginRight: "20px" }}
+                onClick={handleBudimpeštaClick}
+              >
+                Budimpešta
+              </button>
+              <button className="btn btn-primary" onClick={handleRimClick}>
+                Rim
+              </button>
             </td>
           </tr>
           <tr>
             <th scope="row">Mobilnosti učenika</th>
             <td>
-              <button className="btn btn-primary" style={{ marginRight: "20px" }} onClick={handleŠpanjolskaClick}>Španjolska</button>
-              <button className="btn btn-primary" onClick={handleItalijaClick}>Italija</button>
+              <button
+                className="btn btn-primary"
+                style={{ marginRight: "20px" }}
+                onClick={handleŠpanjolskaClick}
+              >
+                Španjolska
+              </button>
+              <button className="btn btn-primary" onClick={handleItalijaClick}>
+                Italija
+              </button>
             </td>
           </tr>
           <tr>
             <th scope="row">Job shadowing</th>
             <td>
-              <textarea className="form-control" value={jobShadowing} onChange={(e) => setJobShadowing(e.target.value)} />
+              <textarea
+                className="form-control"
+                value={jobShadowing}
+                onChange={(e) => setJobShadowing(e.target.value)}
+              />
             </td>
           </tr>
           <tr>
             <th scope="row">Aktivnosti</th>
             <td>
-              <textarea className="form-control" rows="20" value={aktivnosti} onChange={(e) => setAktivnosti(e.target.value)} />
+              <textarea
+                className="form-control"
+                rows="20"
+                value={aktivnosti}
+                onChange={(e) => setAktivnosti(e.target.value)}
+              />
             </td>
           </tr>
           <tr>
             <th scope="row">Diseminacija</th>
             <td>
-              <textarea className="form-control" rows="20" value={diseminacija} onChange={(e) => setDiseminacija(e.target.value)} />
+              <textarea
+                className="form-control"
+                rows="20"
+                value={diseminacija}
+                onChange={(e) => setDiseminacija(e.target.value)}
+              />
             </td>
           </tr>
           <tr>
             <th scope="row">Ostalo</th>
             <td>
-              <textarea className="form-control" value={ostalo} onChange={(e) => setOstalo(e.target.value)} />
+              <textarea
+                className="form-control"
+                value={ostalo}
+                onChange={(e) => setOstalo(e.target.value)}
+              />
             </td>
           </tr>
         </tbody>
       </table>
-      <button className="btn btn-primary" onClick={saveDataToServer}>Spremi podatke na poslužitelj</button>
+      <button className="btn btn-primary" onClick={saveDataToServer}>
+        Spremi podatke na poslužitelj
+      </button>
     </div>
   );
 };
